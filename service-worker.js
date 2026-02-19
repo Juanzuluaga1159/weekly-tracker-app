e121e7ec644a51a23a6aa360ea6c0dadd98af873
Juanzuluaga1159 <Juancho101002@gmail.com>
2026-02-19 13:59:42 -0300

    Update service-worker.js
    optimizacion

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,43 @@
-const CACHE_NAME = "weekly-tracker-cache-v1";
+const CACHE_NAME = "weekly-tracker-cache-v3";
+
 const urlsToCache = [
+  "./",
   "index.html",
-  "manifest.json"
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
+// INSTALACIÓN
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
+// ACTIVACIÓN (borra versiones viejas)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
 });
+
